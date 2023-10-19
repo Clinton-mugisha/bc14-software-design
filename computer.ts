81f49442541f902e7f23b6d41bbe078943b5e2fa
Mugisha Clinton <mugishaclinton412@gmail.com>
2023-10-19 05:33:10 +0300

    changes in typescript

--- computer.ts
+++ computer.ts
@@ -93,11 +93,11 @@ class Desktop extends Computer implements WiFiSupportedDevice, BluetoothSupporte
     // We can add extra functionality to this class
     balance(){}
     connectToWIFI(){
-        console.log('Connecting to wifi.');
+        console.log(' Desktop Connecting to wifi.');
     }
 
     connectToBleatooth(){
-        console.log('Connecting to Bluetooth.');
+        console.log(' Deasktop Connecting to Bluetooth.');
     }
 }
 
@@ -105,11 +105,11 @@ class Laptop extends Computer implements WiFiSupportedDevice, BluetoothSupported
     // We can add extra functionality to this class   
     fold(){}
     connectToWIFI(){
-        console.log('Connecting to wifi.');
+        console.log(' Laptop Connecting to wifi.');
     }
 
     connectToBleatooth(){
-        console.log('Connecting to Bluetooth.');
+        console.log('Laptop Connecting to Bluetooth.');
     }
 }
 
@@ -118,11 +118,11 @@ class Walltop extends Computer implements WiFiSupportedDevice, BluetoothSupporte
     // We can add extra functionality to this class   
     hang(){}
     connectToWIFI(){
-        console.log('Connecting to wifi.');
+        console.log('Walltop Connecting to wifi.');
     }
 
     connectToBleatooth(){
-        console.log('Connecting to Bluetooth.');
+        console.log('Walltop Connecting to Bluetooth.');
     }
 }
 
@@ -130,11 +130,11 @@ class SmartPhone extends Computer implements WiFiSupportedDevice, BluetoothSuppo
     // We can add extra functionality to this class   
     screenTouch(){}
     connectToWIFI(){
-        console.log('Connecting to wifi.');
+        console.log('SmartPhone Connecting to wifi.');
     }
 
     connectToBleatooth(){
-        console.log('Connecting to Bluetooth.');
+        console.log('Smartphone Connecting to Bluetooth.');
     }
 }
 
@@ -144,26 +144,49 @@ class OldModelComputer extends Computer implements BluetoothSupportedDevice{
     }
 
     connectToBleatooth(){
-        console.log('Connecting to Bluetooth.');
+        console.log('OldPC Connecting to Bluetooth.');
     }
 } 
 
 
-// Computer Objects
-let computer1 = new Computer(new Keyboard());
+// // Computer Objects
+// let computer1 = new Computer(new Keyboard());
 
-// Change inpute device dynamically using a setter
-computer1.setInputDevice(new Mouse());
+// // Change inpute device dynamically using a setter
+// computer1.setInputDevice(new Mouse());
 
-// Access the value of a private field using a getter
-console.log(computer1.getInputDevice());
+// // Access the value of a private field using a getter
+// console.log(computer1.getInputDevice());
 
 // Tests
-function testComputer(computer:Computer){
-    computer.input();
-    computer.process();
-    computer.store("any thing");
-    computer.retrieve("name");
-    computer.output("just");
-}
-testComputer(computer1);
+// function testComputer(computer:Computer){
+//     computer.input();
+//     computer.process();
+//     computer.store("any thing");
+//     computer.retrieve("name");
+//     computer.output("just");
+// }
+// testComputer(computer1);
+const keyboard = new Keyboard();
+const desktop = new Desktop(keyboard);
+desktop.connectToWIFI();
+desktop.connectToBleatooth();
+
+const laptop = new Laptop(keyboard);
+laptop.connectToWIFI();
+laptop.connectToBleatooth();
+
+
+const walltop = new Walltop(keyboard);
+walltop.connectToWIFI();
+walltop.connectToBleatooth();
+
+const smartPhone = new SmartPhone(keyboard);
+smartPhone.connectToWIFI();
+smartPhone.connectToBleatooth();
+
+const oldModelComputer = new OldModelComputer(keyboard);
+oldModelComputer.connectToBleatooth();
+
+
+
